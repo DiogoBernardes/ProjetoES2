5d4fce5db042a5cb4f03be0c4236331530e5b91c
Diogo Bernardes <Diogobernardes@ipvc.pt>
2023-06-18 00:32:51 +0100

    event_ticket change

--- BusinessLogic/Entities/event_ticket.cs
+++ BusinessLogic/Entities/event_ticket.cs
@@ -6,10 +6,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BusinessLogic.Entities;
 
-[Keyless]
 [Table("event_ticket")]
 public partial class event_ticket
 {
+    [Key]
+    public Guid id { get; set; }
+
     public Guid ticket_type { get; set; }
 
     public Guid event_id { get; set; }
@@ -17,11 +19,13 @@ public partial class event_ticket
     public int quantity { get; set; }
 
     [Precision(10, 2)]
-    public decimal price { get; set; }
+    public float price { get; set; }
 
     [ForeignKey("event_id")]
+    [InverseProperty("event_tickets")]
     public virtual event_info _event { get; set; } = null!;
 
     [ForeignKey("ticket_type")]
+    [InverseProperty("event_tickets")]
     public virtual ticket_type ticket_typeNavigation { get; set; } = null!;
 }
