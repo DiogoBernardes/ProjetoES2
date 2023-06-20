3f5da2882530ea08f019fd4000c1393e75536aae
Nx-7 <96266508+Nx-7@users.noreply.github.com>
2023-06-20 23:02:09 +0100

    Added EditProfile and Styles

---
+++ BusinessLogic/Models/Event/regist/CreateEventRegistModal.cs
@@ -0,0 +1,12 @@
+namespace BusinessLogic.Models.Event.regist; 
+
+public class CreateEventRegistModal {
+
+    public Guid Event_ID { get; set; }
+    public Guid Participant_ID { get; set; }
+    public Guid State_ID { get; set; }
+    public Guid Ticket_Type_ID { get; set; }
+    
+    
+
+}
